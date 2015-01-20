45d455c2488a9def38788f208c76d410715c6c75
Rakatak <robin-steller@web.de>
2015-01-20 14:05:28 +0100

    starting last task

---
+++ VSAufgabe3/src/vs1/data/PhoneBook.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+
+/**
+* This Class represents the PhoneBook used for searching
+* The class contains a static final ArrayList<String> as the book
+* @author  Robin Steller
+*/
+
+public final class PhoneBook {
+	
+	public static final ArrayList<String> list = new ArrayList<String>() {{
+		add("Hopkins");
+		add("4711");
+		add("Nathan");
+		add("0815");
+		add("Hanzo");
+		add("4711");
+		add("Hopkins");
+		add("0816");
+		add("Von Knotenknut");
+		add("8008");
+		add("Nüga");
+		add("6565");
+		}};
+}
